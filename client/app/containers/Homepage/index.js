29cf8d0adc1232acbcc5f99358f58a7015dc5a0a
maheshtecheval <mahesh@techevalsoftwares.com>
2024-08-22 23:01:44 +0530

    demo for abhi

--- client/app/containers/Homepage/index.js
+++ client/app/containers/Homepage/index.js
@@ -48,6 +48,36 @@ class Homepage extends React.PureComponent {
             </div>
           </Col>
         </Row>
+        <Row className='flex-row'>
+          <Col xs='12' lg='6' className='order-lg-2 mb-3 px-3 px-md-2'>
+            <div className='home-carousel'>
+              <CarouselSlider
+                swipeable={true}
+                showDots={true}
+                infinite={true}
+                autoPlay={false}
+                slides={banners}
+                responsive={responsiveOneItemCarousel}
+              >
+                {banners.map((item, index) => (
+                  <img key={index} src={item.imageUrl} />
+                ))}
+              </CarouselSlider>
+            </div>
+          </Col>
+          <Col xs='12' lg='3' className='order-lg-1 mb-3 px-3 px-md-2'>
+            <div className='d-flex flex-column h-100 justify-content-between'>
+              <img src='/images/banners/banner-2.jpg' className='mb-3' />
+              <img src='/images/banners/banner-5.jpg' />
+            </div>
+          </Col>
+          <Col xs='12' lg='3' className='order-lg-3 mb-3 px-3 px-md-2'>
+            <div className='d-flex flex-column h-100 justify-content-between'>
+              <img src='/images/banners/banner-2.jpg' className='mb-3' />
+              <img src='/images/banners/banner-6.jpg' />
+            </div>
+          </Col>
+        </Row>
       </div>
     );
   }
